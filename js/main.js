9822992069908f2d48abb565da14567e96c28fc8
Alex DiLiberto <alex@alexdiliberto.com>
2014-06-16 14:30:09 -0400

    Added "burocratik/outdated-browser" plugin.
    https://github.com/burocratik/outdated-browser

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+  outdatedBrowser({
+    bgColor: '#3f3f3f',
+    color: '#e3e3e3',
+    lowerThan: 'IE10'
+  });
+});
+
 $("a.top-link").click(function() {
   $("html, body").animate({ scrollTop: 0 }, 1000);
 });
