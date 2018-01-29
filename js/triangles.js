ff01ba3949c4362c2920268def4679abd5384e7e
Alex DiLiberto <alex@alexdiliberto.com>
2018-01-29 01:23:50 -0500

    removed unused code from js/triangles.js

--- js/triangles.js
+++ js/triangles.js
@@ -224,33 +224,6 @@ var Delaunay;
 
       /* Yay, we're done! */
       return open;
-    },
-    contains: function(tri, p) {
-      /* Bounding box test first, for quick rejections. */
-      if((p[0] < tri[0][0] && p[0] < tri[1][0] && p[0] < tri[2][0]) ||
-         (p[0] > tri[0][0] && p[0] > tri[1][0] && p[0] > tri[2][0]) ||
-         (p[1] < tri[0][1] && p[1] < tri[1][1] && p[1] < tri[2][1]) ||
-         (p[1] > tri[0][1] && p[1] > tri[1][1] && p[1] > tri[2][1]))
-        return null;
-
-      var a = tri[1][0] - tri[0][0],
-          b = tri[2][0] - tri[0][0],
-          c = tri[1][1] - tri[0][1],
-          d = tri[2][1] - tri[0][1],
-          i = a * d - b * c;
-
-      /* Degenerate tri. */
-      if(i === 0.0)
-        return null;
-
-      var u = (d * (p[0] - tri[0][0]) - b * (p[1] - tri[0][1])) / i,
-          v = (a * (p[1] - tri[0][1]) - c * (p[0] - tri[0][0])) / i;
-
-      /* If we're outside the tri, fail. */
-      if(u < 0.0 || v < 0.0 || (u + v) > 1.0)
-        return null;
-
-      return [u, v];
     }
   };
 
@@ -351,18 +324,6 @@ FSS.Vector3 = {
     target[2] = z || 0;
     return this;
   },
-  setX: function(target, x) {
-    target[0] = x || 0;
-    return this;
-  },
-  setY: function(target, y) {
-    target[1] = y || 0;
-    return this;
-  },
-  setZ: function(target, z) {
-    target[2] = z || 0;
-    return this;
-  },
   copy: function(target, a) {
     target[0] = a[0];
     target[1] = a[1];
@@ -375,64 +336,10 @@ FSS.Vector3 = {
     target[2] += a[2];
     return this;
   },
-  addVectors: function(target, a, b) {
-    target[0] = a[0] + b[0];
-    target[1] = a[1] + b[1];
-    target[2] = a[2] + b[2];
-    return this;
-  },
-  addScalar: function(target, s) {
-    target[0] += s;
-    target[1] += s;
-    target[2] += s;
-    return this;
-  },
-  subtract: function(target, a) {
-    target[0] -= a[0];
-    target[1] -= a[1];
-    target[2] -= a[2];
-    return this;
-  },
   subtractVectors: function(target, a, b) {
     target[0] = a[0] - b[0];
     target[1] = a[1] - b[1];
     target[2] = a[2] - b[2];
-    return this;
-  },
-  subtractScalar: function(target, s) {
-    target[0] -= s;
-    target[1] -= s;
-    target[2] -= s;
-    return this;
-  },
-  multiply: function(target, a) {
-    target[0] *= a[0];
-    target[1] *= a[1];
-    target[2] *= a[2];
-    return this;
-  },
-  multiplyVectors: function(target, a, b) {
-    target[0] = a[0] * b[0];
-    target[1] = a[1] * b[1];
-    target[2] = a[2] * b[2];
-    return this;
-  },
-  multiplyScalar: function(target, s) {
-    target[0] *= s;
-    target[1] *= s;
-    target[2] *= s;
-    return this;
-  },
-  divide: function(target, a) {
-    target[0] /= a[0];
-    target[1] /= a[1];
-    target[2] /= a[2];
-    return this;
-  },
-  divideVectors: function(target, a, b) {
-    target[0] = a[0] / b[0];
-    target[1] = a[1] / b[1];
-    target[2] = a[2] / b[2];
     return this;
   },
   divideScalar: function(target, s) {
@@ -445,15 +352,6 @@ FSS.Vector3 = {
       target[1] = 0;
       target[2] = 0;
     }
-    return this;
-  },
-  cross: function(target, a) {
-    var x = target[0];
-    var y = target[1];
-    var z = target[2];
-    target[0] = y*a[2] - z*a[1];
-    target[1] = z*a[0] - x*a[2];
-    target[2] = x*a[1] - y*a[0];
     return this;
   },
   crossVectors: function(target, a, b) {
@@ -479,45 +377,17 @@ FSS.Vector3 = {
     this.max(target, max);
     return this;
   },
-  limit: function(target, min, max) {
-    var length = this.length(target);
-    if (min !== null && length < min) {
-      this.setLength(target, min);
-    } else if (max !== null && length > max) {
-      this.setLength(target, max);
-    }
-    return this;
-  },
   dot: function(a, b) {
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
   },
   normalise: function(target) {
     return this.divideScalar(target, this.length(target));
   },
-  negate: function(target) {
-    return this.multiplyScalar(target, -1);
-  },
-  distanceSquared: function(a, b) {
-    var dx = a[0] - b[0];
-    var dy = a[1] - b[1];
-    var dz = a[2] - b[2];
-    return dx*dx + dy*dy + dz*dz;
-  },
-  distance: function(a, b) {
-    return Math.sqrt(this.distanceSquared(a, b));
-  },
   lengthSquared: function(a) {
     return a[0]*a[0] + a[1]*a[1] + a[2]*a[2];
   },
   length: function(a) {
     return Math.sqrt(this.lengthSquared(a));
-  },
-  setLength: function(target, l) {
-    var length = this.length(target);
-    if (length !== 0 && l !== length) {
-      this.multiplyScalar(target, l / length);
-    }
-    return this;
   }
 };
 
@@ -535,22 +405,6 @@ FSS.Vector4 = {
     target[0] = x || 0;
     target[1] = y || 0;
     target[2] = z || 0;
-    target[3] = w || 0;
-    return this;
-  },
-  setX: function(target, x) {
-    target[0] = x || 0;
-    return this;
-  },
-  setY: function(target, y) {
-    target[1] = y || 0;
-    return this;
-  },
-  setZ: function(target, z) {
-    target[2] = z || 0;
-    return this;
-  },
-  setW: function(target, w) {
     target[3] = w || 0;
     return this;
   },
